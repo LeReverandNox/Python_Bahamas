9c03b929f84f9f184b602ca640d519560a5edf5d
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-21 00:41:32 +0100

    [first-steps] Add a HandleConnection class to handle connection to a client. [#1]

--- Threads/HandleConnection.py
+++ Threads/HandleConnection.py
@@ -1,14 +1,16 @@
 import threading as t
 
 class HandleConnection(t.Thread):
-    def __init__(self, lock):
+    def __init__(self, server, lock, socket):
         t.Thread.__init__(self)
 
+        self.server = server
         self.lock = lock
+        self.socket = socket
         self.isRunning = True
 
     def stop(self):
         self.isRunning = False
 
     def run(self):
-        print('On recois une connexion')
+        print('On recois une connexion du socket {}'.format(self.socket))
