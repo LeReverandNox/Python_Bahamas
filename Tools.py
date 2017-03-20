5ff784d4706e97897db908319248f80d2d0810cb
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-20 21:22:32 +0100

    [first-steps] Add a method on Tools to check if a port is valid. ToDo: add taken / bindable test [#1]

--- Tools.py
+++ Tools.py
@@ -12,3 +12,11 @@ class Tools(metaclass=Singleton):
             return int(nbr)
         except ValueError:
             return 0
+
+    def isPortValid(nbr):
+        port = Tools.castInt(nbr)
+        if port == 0:
+            raise TypeError('Please enter a numeric port value')
+        if port < 1024 or port > 65535:
+            raise ValueError('The port must be in the range 1024-65535')
+        return True
