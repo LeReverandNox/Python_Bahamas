a347ff5e228a81af89e317e1cf636a38614158fe
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-20 21:44:02 +0100

    [first-steps] Improve port checking with a isPortAvailable method [#1]

--- Tools.py
+++ Tools.py
@@ -1,3 +1,5 @@
+import socket
+
 class Singleton(type):
     _instances = {}
     def __call__(self, *args, **kwargs):
@@ -19,4 +21,22 @@ class Tools(metaclass=Singleton):
             raise TypeError('Please enter a numeric port value')
         if port < 1024 or port > 65535:
             raise ValueError('The port must be in the range 1024-65535')
+        if not Tools.isPortAvailable(port):
+            pass
+
         return True
+
+    def isPortAvailable(port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.bind(('0.0.0.0', port))
+        except socket.error as e:
+            s.close()
+            if e.errno == 98:
+                raise ValueError('This port is already taken')
+            else:
+                print(e)
+                raise Exception('Unknow Error, please contact an administrator (Code {})'.format(e.errno))
+        else:
+            s.close()
+            return True
