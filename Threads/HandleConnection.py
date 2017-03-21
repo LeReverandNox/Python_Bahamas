78bd376e25685eb2eeb8dd65bc7b0812b4bc21b3
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-21 15:59:50 +0100

    [first-steps] Begin to handle connection and new clients on server, and receive first message. [#1]

--- Threads/HandleConnection.py
+++ Threads/HandleConnection.py
@@ -1,16 +1,42 @@
 import threading as t
+import json
 
 class HandleConnection(t.Thread):
-    def __init__(self, server, lock, socket):
+    def __init__(self, server, lock, socket, ip):
         t.Thread.__init__(self)
 
         self.server = server
         self.lock = lock
         self.socket = socket
+        self.ip = ip
         self.isRunning = True
 
+        # Create the base Client, with just his socket and public ip.
+        self.server.addClient(socket, ip)
     def stop(self):
         self.isRunning = False
 
+    def parseMessage(self, byteMessage):
+        messageDict = json.loads(byteMessage.decode('utf-8'))
+        action = messageDict['action']
+        data = messageDict['data']
+
+        def actionSwitch(action):
+            switcher = {
+                'welcome': self.server.completeClient
+            }
+            func = switcher.get(action, lambda: "nothing")
+            return func(data, self.socket)
+
+        actionSwitch(action)
+
     def run(self):
-        print('On recois une connexion du socket {}'.format(self.socket))
+        while self.isRunning:
+            try:
+                byteMessage = self.socket.recv(1024)
+            except Exception as e:
+                print('Lien rompu avec {}'.format(self.ip))
+                print(e)
+            else:
+                if byteMessage:
+                    self.parseMessage(byteMessage)
