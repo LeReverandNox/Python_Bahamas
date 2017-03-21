eca3f9f78438df1e13dbb83aedabd9a1e62f26b0
laidet_r <rodolphe.laidet@epitech.eu>
2017-03-21 22:39:59 +0100

    [channels-and-methods] Introduce a protocol to receive message through socket.  Get the length on the firsts 4 bytes, then read [#2]

--- Threads/HandleConnection.py
+++ Threads/HandleConnection.py
@@ -1,6 +1,7 @@
 import threading as t
 import json
 from Threads.Ping import Ping
+import struct
 
 class HandleConnection(t.Thread):
     def __init__(self, server, lock, socket, ip):
@@ -27,21 +28,35 @@ class HandleConnection(t.Thread):
 
         def actionSwitch(action):
             switcher = {
-                'welcome': self.server.completeClient
+                'welcome': self.server.completeClient,
+                'createChannel': self.server.addChannel
             }
             func = switcher.get(action, lambda: "nothing")
             return func(data, self.socket)
 
-        actionSwitch(action)
-
-    def run(self):
-        while self.isRunning:
+        try:
+            actionSwitch(action)
+        except Exception as e:
+            print(str(e))
+    def recvSome(self, length):
+        completeBuffer = b''
+        while length:
             try:
-                byteMessage = self.socket.recv(1024)
+                buffer = self.socket.recv(length)
             except Exception as e:
                 print('Lien rompu avec {}'.format(self.ip))
                 self.stop()
                 self.server.removeClient(self.socket)
             else:
-                if byteMessage:
-                    self.parseMessage(byteMessage)
+                if buffer:
+                    completeBuffer += buffer
+                    length -= len(buffer)
+        return completeBuffer
+
+    def run(self):
+        while self.isRunning:
+            binaryLength = self.recvSome(4)
+            messageLength, = struct.unpack('!I', binaryLength)
+            binaryMessage = self.recvSome(messageLength)
+
+            self.parseMessage(binaryMessage)
